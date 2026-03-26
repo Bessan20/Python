3b08535bee2d00a881e65eb87ea878fcc2ec8530
abc <abc@example.com>
2026-03-26 19:29:29 +0200

    main.py

--- 1-Basics/main.py
+++ 1-Basics/main.py
@@ -1,2 +1,18 @@
-print("hello")
+print("Hello world!")
+print("This is a Python program.")
+
+
 print("bessan")
+print("We school")
+print(16)
+
+name = "bessan"
+age = 16 
+print("My name is : " , name)
+print("My age is : " , age)
+
+x = 6 
+y = 3 
+print("6 + 3 = " , x + y)
+print("6 - 3 = " , x - y)
+print("6 * 3 = " , x * y)
